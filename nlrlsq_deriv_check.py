4ce0399946e45933d5c2b5e978bb140906e381c3
wallacerosalind <rosalindwfinley@gmail.com>
2024-02-26 13:33:03 -0600

    deriv check runs but all the errors are too large and all linear in h

---
+++ nlrlsq_deriv_check.py
@@ -0,0 +1,51 @@
+import numpy as np
+import sys
+sys.path.append("..")
+from LineSearchOpt import *
+
+n = 10 #col A
+m = 12 #rows A
+A = np.random.rand(m, n)
+L = np.random.rand(n, n)
+x = np.random.rand(n)
+b = np.random.rand(m)
+AT = A.transpose()
+LT = L.transpose()
+xT = x.transpose()
+bT = b.transpose()
+
+# evaluate objective function
+def eval_objfun( A, L, x, b, beta, flag="d2f" ):
+
+    # evaluate objective function
+    f = 0.5*np.square(np.linalg.norm( np.sin(np.matmul(A, x)) - b ))\
+            + beta*0.5*np.square(np.linalg.norm(np.matmul(L, x)))
+
+    if flag == "f":
+        return f
+
+    # evaluate gradient
+    df =
+
+    if flag == "df":
+        return f,df
+
+    #n = A.shape[0];
+    # evaluate hessian
+    d2f =
+
+    return f,df,d2f;
+
+
+# initialize class
+opt = Optimize();
+
+# define function handle
+fctn = lambda x, flag: eval_objfun( A, L, x, b, 0.1, flag )
+
+# set objective function
+opt.set_objfctn( fctn )
+
+# perform derivative check
+opt.deriv_check( x )
+
